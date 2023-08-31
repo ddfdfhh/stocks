2c8d1336ad78fe9e87cb8961b35a890f054c6124
ddfdfhh <shashi801@gmail.com>
2023-08-31 12:39:39 +0530

    added .htf3ddd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,8 @@
     Login
 @endsection
 @section('content')
- <h4 class="mb-2">Welcome</h4>
-          <p class="mb-4">Please sign-in to your account</p>
+ 
+          <p class="mb-4 text-center">Please sign-in to your account</p>
   <div class="login_error_msg" style="color:red"></div>
    <form  class="mb-3" action="{{ route('login') }}" id="login_form" method="POST">
    @csrf
