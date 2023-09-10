0679ecda5eaf1f814ab461381b6ae9a7bd629745
ddfdfhh <shashi801@gmail.com>
2023-09-10 17:52:41 +0530

    dfdsdddddhjd

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -21,22 +21,23 @@
         <!-- Basic Bootstrap Table -->
         <div class="card">
             <div class="card-header">
-                <div class="d-flex justify-content-between flex-wrap">
+                 <div class="d-flex justify-content-between flex-wrap">
                     <h5>All {{ properPluralName($plural_lowercase) }}</h5>
                     <div class="d-flex">
-
                         <div class="btn-group" role="group" aria-label="Basic example">
                             @if (auth()->user()->hasRole(['Admin']) ||
                                     auth()->user()->can('create_' . $plural_lowercase))
-                               <button class="btn btn-primary" type="button"
-                                    onclick="load_form('{!! $module !!}','add','{!! route(strtolower($module) . '.loadAjaxForm') !!}',null,'{!! properSingularName($plural_lowercase) !!}')"
-                                    aria-controls="offcanvasEnd"> <i class="fa fa-plus-circle"></i>  Add
-                                        New</button>
+                                <button type="button" class="rounded-0 btn btn-primary text-white">
+                                    <a href="{{ route($plural_lowercase . '.create') }}"
+                                        class="text-decoration-none text-white">
+                                       <i class="bx bx-plus-circle" style="margin-top:-3px"></i>  Add New 
+                                        </a>
+                                </button>
                             @endif
 
                             @if ($has_export)
                                 <button type="button"
-                                    class="dt-button buttons-collection btn btn-label-primary dropdown-toggle me-2"
+                                    class="rounded-0 dt-button buttons-collection btn btn-label-primary dropdown-toggle me-2"
                                     data-bs-toggle="dropdown" aria-expanded="false">
                                     <span><i class="bx bx-export me-sm-2"></i> <span
                                             class="d-none d-sm-inline-block">Export</span></span>
@@ -60,7 +61,18 @@
                             @endif
 
                         </div>
+
                     </div>
+                </div>
+                
+                  <br>
+                <div class="d-flex justify-content-between flex-wrap mt-3">
+                    <div class="d-flex flex-wrap justify-content-between " style="align-items: start;max-width:660px; ">
+                       
+                        <x-filter :data="$filterable_fields" />
+                    </div>
+                    <x-search :searchableFields="$searchable_fields" />
+
                 </div>
              
 
